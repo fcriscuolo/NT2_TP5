d9f2e8027759b0a525da037dfe9f4da2e2590bcf
Fede <fcriscuolo@doscclicks.com.ar>
2021-11-15 17:01:07 -0300

    Entrega TP5

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Colors/>
+  </div>
+</template>
+
+<script>
+import Colors from './components/Colors.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Colors
+  }
+}
+</script>
+
+<style>
+
+</style>
